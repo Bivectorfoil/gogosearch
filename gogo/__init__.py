68c7fb88466a609c81690caa64320ec94b85b621
Bivectorfoil <550282600@qq.com>
2018-10-20 10:56:27 +0800

    Update init file for secret

--- gogo/__init__.py
+++ gogo/__init__.py
@@ -1,9 +1,11 @@
 # -*- coding:utf-8 -*-
 
+import os
+
 from flask import Flask
 
 
 app = Flask('gogo')
-app.config['SECRET_KEY'] = 'hard to guess string'
+app.secret_key = os.getenv('SECRET_KEY', 'hard to guess string')
 
 from gogo import views
