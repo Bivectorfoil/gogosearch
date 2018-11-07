40d93318cb739e4f86372854510fa8861f542a1d
Bivectorfoil <550282600@qq.com>
2018-11-07 21:21:35 +0800

    Fix nginx configuration IP addr issue

--- setup.sh
+++ setup.sh
@@ -33,6 +33,14 @@ echo "Install requirements from pipfile..."
 pipenv install
 
 # setup nginx
+get_ip(){
+    local IP=$( ip addr | egrep -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | egrep -v "^192\.168|^172\.1[6-9]\.|^172\.2[0-9]\.|^172\.3[0-2]\.|^10\.|^127\.|^255\.|^0\." | head -n 1 )
+    [ -z ${IP} ] && IP=$( wget -qO- -t1 -T2 ipv4.icanhazip.com )
+    [ -z ${IP} ] && IP=$( wget -qO- -t1 -T2 ipinfo.io/ip )
+    [ ! -z ${IP} ] && echo ${IP} || echo
+}
+IP=$(get_ip)
+
 touch nginx.conf
 
 static_dir=`pwd`"/gogo/static/"
@@ -40,7 +48,7 @@ cat << EOF > ./nginx.conf
 server {
 	listen 80;
 
-    server_name _;  # change it if you have domain
+    server_name $IP;  # change it if you have domain
 	access_log /var/log/nginx/access.log;
 	error_log /var/log/nginx/error.log;
 
