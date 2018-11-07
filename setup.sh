d6a415ebec1cd4339488728d22f82845164afc82
Bivectorfoil <550282600@qq.com>
2018-11-07 19:46:49 +0800

    Update setup, intergration nginx configuration

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Develop script for Flask Web App, with gunicorn and nginx
-# run it as root
+# run it as root or sudo
 
 set -e
 
@@ -26,6 +26,33 @@ echo "Install requirements from pipfile..."
 pipenv install
 
 # setup nginx
+touch nginx.conf
+
+static_dir=`pwd`"/gogo/static/"
+cat << EOF > ./nginx.conf
+server {
+	listen 80;
+
+    server_name _;  # change it if you have domain
+	access_log /var/log/nginx/access.log;
+	error_log /var/log/nginx/error.log;
+
+	location / {
+		proxy_pass http://127.0.0.1:8000; # gunicorn run port
+		proxy_redirect off;
+
+		proxy_set_header Host               $host;
+		proxy_set_header X-Real-IP          $remote_addr;
+		proxy_set_header X-Forwarded-For    $proxy_add_x_forwarded_for;
+		proxy_set_header X-Forwarded-Proto  $scheme;
+	}
+	location /static {
+		alias $static_dir;
+		expires 30d;
+	}
+}
+EOF
+
 if [ "`ls -A /etc/nginx/sites-enabled/`" != "" ]; then
     rm /etc/nginx/sites-enabled/*
 fi
