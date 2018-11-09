38e766b624a7d42e63ccba1bbf99c005e9282c4e
Bivectorfoil <550282600@qq.com>
2018-11-09 22:32:21 +0800

    Fix tab indentation issue

--- setup.sh
+++ setup.sh
@@ -44,24 +44,24 @@ touch nginx.conf
 static_dir=`pwd`"/gogo/static/"
 cat << EOF > ./nginx.conf
 server {
-	listen 80;
+    listen 80;
     server_name $IP;
-	access_log /var/log/nginx/access.log;
-	error_log /var/log/nginx/error.log;
-
-	location / {
-		proxy_pass http://127.0.0.1:8000; # gunicorn run port
-		proxy_redirect off;
-
-		proxy_set_header Host               \$host;
-		proxy_set_header X-Real-IP          \$remote_addr;
-		proxy_set_header X-Forwarded-For    \$proxy_add_x_forwarded_for;
-		proxy_set_header X-Forwarded-Proto  \$scheme;
-	}
-	location /static {
-		alias $static_dir;
-		expires 30d;
-	}
+    access_log /var/log/nginx/access.log;
+    error_log /var/log/nginx/error.log;
+    
+    location / {
+        proxy_pass http://127.0.0.1:8000; # gunicorn run port
+        proxy_redirect off;
+        
+        proxy_set_header Host               \$host;
+        proxy_set_header X-Real-IP          \$remote_addr;
+        proxy_set_header X-Forwarded-For    \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto  \$scheme;
+    }
+    location /static {
+        alias $static_dir;
+        expires 30d;
+    }
 }
 EOF
 
