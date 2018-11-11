9951683ad2384c84ae07cbf40b558887b61e03cc
Bivectorfoil <550282600@qq.com>
2018-11-11 20:59:39 +0800

    Change dockerfile to copy whole app

--- dockerfile
+++ dockerfile
@@ -2,13 +2,7 @@ FROM python:2.7.15-alpine3.8
 
 RUN mkdir -p /usr/src/app  && mkdir -p /var/log/gunicorn
 
-# COPY . /usr/src/app
-COPY .env /usr/src/app/
-COPY .flaskenv /usr/src/app/
-
-# copy code to src
-COPY gogo/ /usr/src/app/
-COPY requirements.txt /usr/src/app/
+COPY . /usr/src/app
 
 WORKDIR /usr/src/app
 
--- setup_docker.sh
+++ setup_docker.sh
@@ -38,9 +38,8 @@ apt-get update && apt-get install -y docker-ce
 docker version
 
 # Install docker Compose
-curl -L
-"https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname
--s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L \
+    "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 chmod +x /usr/local/bin/docker-compose
 
